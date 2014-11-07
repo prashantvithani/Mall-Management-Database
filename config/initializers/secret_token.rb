013ac6f43d7b16d2379bbde9a7f9b5d979401681
Prashant Vithani <prashantvithani@gmail.com>
2014-11-08 04:21:52 +0530

    Initialize repository for mall_management_database.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MallManagement::Application.config.secret_key_base = '1d3a9da4082f5f9e951c5e06be10618bfb11e0c2645b81568bb78c1c7caced9e1dbf0ee6a82c7fe5a08eee2656c4ae98149759c036c645881251fe3dfcbd972f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MallManagement::Application.config.session_store :cookie_store, key: '_MallManagement_session'
